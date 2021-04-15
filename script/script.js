b4ffd64d0c6bcebf4638f7611bfcf099c136987c
Brendan Tyler <bkrendan12@gmail.com>
2021-04-15 17:42:45 -0400

    finished all functionality other than fixing the splicing, not allowing repeats searches, and styling issues

--- script/script.js
+++ script/script.js
@@ -1,12 +1,25 @@
 var searchBtn = $(".btn");
 var savedCityBtn = $(".btnCity");
-var resultsPage = $("#results-container");
 var cityText = $("#inputCity");
+var fiveDay = $(".fiveDay");
+var icons = $(".weather-icon");
 
-function getApi() {
+var searchedCities = [];
+var historyUl = $("#historyUl");
+
+var localGet = localStorage.getItem("City Name");
+
+if (localGet !== null) {
+  searchedCities = JSON.parse(localGet);
+  console.log(searchedCities);
+}
+showButtons();
+
+function getApi(cityName) {
   return new Promise(function (resolve, reject) {
     fetch(
-      `https://api.openweathermap.org/data/2.5/forecast?q=${cityText.val()}&appid=045656fed699b763d7b54a4eb7137c4e&units=imperial`
+      //forecastData API for everything excluding uv index
+      `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=045656fed699b763d7b54a4eb7137c4e&units=imperial`
     )
       .then(function (response) {
         return response.json();
@@ -16,6 +29,7 @@ function getApi() {
         var lon = forecastData.city.coord.lon;
 
         fetch(
+          //onecalldata api for uv index and 5-day Forecast
           `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly,daily,alerts&appid=045656fed699b763d7b54a4eb7137c4e&units=imperial`
         )
           .then(function (response) {
@@ -33,9 +47,18 @@ function getApi() {
 
 searchBtn.on("click", function (event) {
   event.preventDefault();
+  getWeatherData(cityText.val());
+  // check for repeat
+  searchedCities.push(cityText.val());
+  count = 0;
+  // if array is too long, cut it down if higher than specifized number, splice method
+  localStorage.setItem("City Name", JSON.stringify(searchedCities));
+});
 
-  getApi().then(function (data) {
+function getWeatherData(cityName) {
+  getApi(cityName).then(function (data) {
     console.log(data);
+
     // current city weather data
     $("#city").text(
       `City: ${data.forecastData.city.name} (${moment().format("M/D/YYYY")})`
@@ -43,21 +66,94 @@ searchBtn.on("click", function (event) {
     $("#temp").text(`Temp: ${data.oneCallData.current.temp} ºF`);
     $("#wind").text(`Wind: ${data.oneCallData.current.wind_speed} mph`);
     $("#humidity").text(`Humidity: ${data.oneCallData.current.humidity} %`);
-    $("#UV-index").text(`UV Index: ${data.oneCallData.current.uvi}`);
-    // 5-day forecast data
-    for (i = 0; i < data.forecastData.list.length; i++) {
-      // ------------
-      $(".fiveDay").text(
-        `Date: ${moment().format("M/D/YYYY")}
-         Icon: ${data.forecastData.list[i].weather[i].icon}
-         Temp: ${data.forecastData.list[i].main.temp} ºF 
-         Wind: ${data.forecastData.list[i].wind.speed}
-         Mph Humidity: ${data.forecastData.list[i].main.humidity}%`
-      );
+    //
+    var uvEl = $("#UV-index");
+    var uv = data.oneCallData.current.uvi;
+    uvEl.text(`UV Index: ${uv}`);
+    uvEl = uvEl.next();
+    if (uv < 6) {
+      uvEl.addClass("favorable");
+      uvEl.removeClass("severe");
+      uvEl.removeClass("moderate");
+    } else if (uv > 7) {
+      uvEl.addClass("severe");
+      uvEl.removeClass("favorable");
+      uvEl.removeClass("moderate");
+    } else {
+      uvEl.addClass("moderate");
+      uvEl.removeClass("favorable");
+      uvEl.removeClass("severe");
     }
+
+    showButtons();
+
+    // 5-day forecast data
+    fiveDay.each((index, element) => {
+      element = $(element);
+      index = index;
+
+      element.empty(); // empty() removes all of the present content inside of the fiveDay div from the palceholder-html
+      var dateDiv = $("<div>");
+      dateDiv.text(`Date: ${moment().add(index, "days").format("M/D/YYYY")}`);
+      element.append(dateDiv);
+
+      var tempDiv = $("<div>");
+      tempDiv.text(`Temp: ${data.forecastData.list[index].main.temp} ºF`);
+      element.append(tempDiv);
+
+      var windDiv = $("<div>");
+      windDiv.text(`Wind: ${data.forecastData.list[index].wind.speed} Mph`);
+      element.append(windDiv);
+
+      var humidityDiv = $("<div>");
+      humidityDiv.text(
+        `Humidity: ${data.forecastData.list[index].main.humidity}%`
+      );
+      element.append(humidityDiv);
+
+      var imgEl = document.createElement("img");
+
+      imgEl.setAttribute(
+        "src",
+        `http://openweathermap.org/img/w/${data.forecastData.list[index].weather[0].icon}.png`
+      );
+
+      element.append(imgEl);
+
+      var descriptionDiv = $("<div>");
+      humidityDiv.text(
+        `${data.forecastData.list[index].weather[0].description}`
+      );
+      element.append(descriptionDiv);
+    });
   });
+}
+
+function showButtons() {
+  historyUl.empty();
+  for (var i = 0; i < searchedCities.length; i++) {
+    var liBtn = $(`
+    <li>
+    <button type="button" data-city="los-angeles" class="btnCity" padding: 5px;>
+      ${searchedCities[i]}
+    </button>
+  </li>`);
+    historyUl.append(liBtn);
+
+    if (searchedCities.length > 8) {
+      searchedCities.splice(i, 1);
+    }
+  }
+}
+
+historyUl.on("click", "button", function () {
+  console.log($(this).text());
+  var cityName = $(this).text();
+  getWeatherData(cityName);
 });
 
-savedCityBtn.on("click", function () {
-  alert("city button working");
-});
+// WHEN I click on a city in the search history
+// THEN I am again presented with current and future conditions for that city
+// Uses `localStorage` to store search history.
+// increase icon size/text size inside of line 97 setAttribute
+// center saved buttons
